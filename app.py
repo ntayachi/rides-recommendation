f6c83ae1bacbf1380a425f803ba84dad8b7e2590
Nafaa.Tayachi <tayachi.nafaa@gmail.com>
2021-04-17 18:10:20 +0100

    fetch weather conditions from api

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+import requests
+import config
+import json
 
 app = Flask(__name__)
 
@@ -6,16 +9,25 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         sunday_date = request.form['sunday_date']
-        dest = calculateDestination(sunday_date)
-        return redirect(url_for('destination', destination=dest))
+        return redirect(url_for('destination', date=sunday_date))
     return render_template('index.html')
 
-@app.route('/destination/<string:destination>')
-def destination(destination):
-    return render_template('destination.html', destination=destination)
+@app.route('/destination/<date>')
+def destination(date):
+    dest = calculateDestination(date)
+    return render_template('destination.html', destination=dest)
 
 def calculateDestination(sunday_date):
-    return 'Tunis'
+    api_url = f'{config.API_BASE_URL}/current.json?key={config.API_KEY}&q=TUN'
+    weather = requests.get(api_url).content
+    weather_json = json.loads(weather)
+    result = {
+        'City': weather_json['location']['name'],
+        'Wind Direction': weather_json['current']['wind_dir'],
+        'Wind Speed': weather_json['current']['wind_kph'],
+        'Temperature': weather_json['current']['temp_c']
+    }
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
